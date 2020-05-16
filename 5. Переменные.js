cf25b89eefd9b253cc12a78cfad441c6abe4f810
ArthurKuchuk <arthur.bearnyson@gmail.com>
2020-05-16 14:02:06 +0600

    Js learn

---
+++ 5. Переменные.js
@@ -0,0 +1,28 @@
+"use strict";
+// Переменные существую для хранения информации, в переменных информация может меняться постоянно.
+
+// Объявление переменной
+
+let userName = "Имя Пользователя";
+
+
+// Разные стили обьявления переменной
+
+let randomNameOne = 1
+  , randomNameTwo = 2
+  , summaryNames = randomNameOne + randomNameTwo;
+
+let randomVar1001 = 1001;
+let randomVar1002 = 1002;
+let randomVar1003 = 1003;
+let randomSummary = randomVar1001 + randomVar1002 + randomVar1003;
+
+// Константы, их значение не меняется.
+const MY_BIRTH_DAY = "01.05.1999";
+const pageLoadTime = "xxx";
+
+// В верхнем регистре пишутся значения которые известны до выполнения скрипта, в нажнем пишутся те, которые будут полученны после выполнения скрипта.
+alert(pageLoadTime);
+
+
+
